70e1e07e9c32440ac33e8ec876f29efc2a917811
Gray <lapenkov.sergey@gmail.com>
2015-03-29 00:56:13 +0200

    Create maniform. Add tree with some nodes and subnodes.

--- src/HumidityMeter.java
+++ src/HumidityMeter.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
 
 /**
  * Created by Gray on 28.03.2015.
@@ -9,10 +10,32 @@ public class HumidityMeter extends JFrame {
 
     public HumidityMeter() {
 
+        //Tree
+        //root node
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode();
+
+        //sensor 1 node
+        DefaultMutableTreeNode sensor1 = new DefaultMutableTreeNode("Sensor 1");
+        //subnode
+        DefaultMutableTreeNode board1 = new DefaultMutableTreeNode("Board 1");
+        sensor1.add(board1);
+
+        //sensor 2 node
+        DefaultMutableTreeNode sensor2 = new DefaultMutableTreeNode("Sensor 2");
+        //subnode
+        DefaultMutableTreeNode board2 = new DefaultMutableTreeNode("Board 2");
+        sensor2.add(board2);
+
+        root.add(sensor1);
+        root.add(sensor2);
+
+        tree = new JTree(root);
+        add(tree);
+
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Humidity Meter");
-        this.setSize(275, 100);
-        //this.pack();
+        this.pack();
         this.setVisible(true);
     }
 
